1b921d1797d9717b3d3b4b64b18034c0715b796b
fstempler <zackmango@gmail.com>
2023-12-19 17:57:53 -0300

    Update Projects / Fix Project Cards Links

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,8 @@ import klhome from '../../assets/Cards/cards__klhome.jpg';
 import klventures from '../../assets/Cards/cards__klventures.jpg';
 import silmar from '../../assets/Cards/cards_silmar.jpg';
 import memoryTest from '../../assets/Cards/cards__MemoryTest.jpg';
-import spincity from '../../assets/Cards/cards__spincity.jpg'
+import spincity from '../../assets/Cards/cards__spincity.jpg';
+import gumat from '../../assets/Cards/cards__Gumat.jpg';
 
 import CardRight from './Components/Cards/CardRight';
 
@@ -18,14 +19,24 @@ const Project = () => {
                 </h1>
             </div>
             <div className='project__Cards-Container'>
+                
+                <CardRight 
+                backgroundImage={gumat}
+                title='GUMAT'
+                text='A Home Page for a company that manufactures LED technology. 
+                
+                Available in Vercel to see on-line.'
+                tools='HTML | CSS | BOOTSTRAP | REACT JS | GIT '
+                linkContent={'https://gumat.com.ar/'}/>
 
                 <CardRight 
                 backgroundImage={klhome}
                 title='KRAUSS & LIVIC'
-                text='A Home Page for a international 
+                text='A Home Page for an international 
                 management company. 
                 
                 Available in Vercel to see on-line.'
+                linkContent={'https://www.krausslivic.com/'}
                 tools='HTML | CSS | BOOTSTRAP | REACT JS | GIT '/>
 
                 <CardRight
@@ -35,6 +46,7 @@ const Project = () => {
                 capital firm with its own software 
                 development company, focused on 
                 propelling the growth of early-stage tech start-ups.'
+                linkContent={'https://www.krausslivic.ventures/'}
                 tools='HTML | CSS | BOOTSTRAP | REACT JS | GIT '/>
 
                 <CardRight
@@ -43,6 +55,8 @@ const Project = () => {
                 text='A sales app built for a local bakery that needed 
                 a better tool to instantly give their 
                 prices to their clients.'
+                linkGithub={'https://github.com/fstempler/silmarCalculadoraPedidos'}
+                linkContent={'https://fstempler.github.io/silmarCalculadoraPedidos/'}
                 tools='HTML | CSS | BOOTSTRAP | JS | GIT '/>
 
                 <CardRight
@@ -51,6 +65,8 @@ const Project = () => {
                 text='A simple EMOJI memory test game in synthwave style. 
                 
                 Available in Vercel to see on-line.'
+                linkContent={'https://memo-test-kappa.vercel.app/'}
+                linkGithub={'https://github.com/fstempler/memoTest'}
                 tools='HTML | CSS | BOOTSTRAP | JS | GIT '/>
 
                 <CardRight
@@ -60,6 +76,8 @@ const Project = () => {
                 available  on my GitHub profile.
                 
                 Available in Vercel to see on-line.'
+                linkContent={'https://proyecto-react-stempler.vercel.app/'}
+                linkGithub={'https://github.com/fstempler/proyectoReact-Stempler'}
                 tools='HTML | CSS | BOOTSTRAP | REACT JS | GIT '/>
 
             </div>
